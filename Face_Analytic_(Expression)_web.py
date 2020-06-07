4e4b349a49424bf5c10d2c41d0aeb67268c9ffa6
I Made Agus Dwi Suarjaya <i.made.agus.dwi.suarjaya@gmail.com>
2020-06-07 17:59:10 +0800

    Modified web codes and Added tflite

--- Face_Analytic_(Expression)_web.py
+++ Face_Analytic_(Expression)_web.py
@@ -8,12 +8,14 @@ Dataset source  : https://www.kaggle.com/jonathanoheix/face-expression-recogniti
 """
 import io
 import ast
+import time
 import base64
 import numpy as np
 import tensorflow as tf
 
 from flask import Flask
 from flask import request
+from time import gmtime, strftime
 from keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
@@ -80,9 +82,35 @@ homepage_end = '''
 </body>
 </html>
 '''
+resp = {
+                    'angry':'Why you angry, keep calm..',
+                    'disgust':'Do you think I am digusting?',
+                    'fear':'Dont be afraid, I am here with you!',
+                    'happy':'Yay, I am happy too!',
+                    'neutral':'You look okay..',
+                    'sad':'Owh, dont be sad please..',
+                    'surprise':'Why so surprised? Haha'
+                    }
 #-----------------------------------
 # Flask codes for web service
 #-----------------------------------
+@app.route("/stream", methods=['POST'])
+def stream():
+    p_result = ''
+    p_images = ''
+    predict_result = ''
+    uploaded_files = request.files.getlist("file")
+
+    if len(uploaded_files) > 0:
+        txt = str(uploaded_files[0])
+        if txt.find('application/octet-stream') == -1:
+            for id, cfile in enumerate(uploaded_files):
+                if id >=1: break
+                predict_result = resp.get(predict_face(cfile))
+
+    return {'blob_name': str(time.time()), 'expression': predict_result, 'image_public_url':  None,
+            'timestamp': strftime("%a, %d %b %Y %H:%M:%S", gmtime())}
+
 @app.route("/", methods=['POST', 'GET'])
 def home():
     p_result = ''
@@ -109,4 +137,4 @@ def home():
     return homepage_head + homepage_body + predict_result + homepage_end
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=80)
+    app.run(host='127.0.0.1', port=443, ssl_context='adhoc')
