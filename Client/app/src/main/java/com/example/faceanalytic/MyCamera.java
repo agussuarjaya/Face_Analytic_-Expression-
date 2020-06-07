4e9b452bdc4fe6376bb4dfea51056cb81ca29665
Adi Putra <tu_adi32@yahoo.com>
2020-06-07 17:47:54 +0800

    adi commit android

---
+++ Client/app/src/main/java/com/example/faceanalytic/MyCamera.java
@@ -0,0 +1,223 @@
+package com.example.faceanalytic;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.os.Environment;
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.faceanalytic.apiHelper.BaseApiService;
+import com.example.faceanalytic.apiHelper.UtilsApi;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MyCamera extends Activity {
+    private Camera mCamera;
+    private CameraPreview mCameraPreview;
+    private TextView tvText;
+    private BaseApiService mApiService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mApiService = UtilsApi.getAPIService();
+        mCamera = getCameraInstance();
+        mCameraPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mCameraPreview);
+        tvText = findViewById(R.id.txt);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                mCamera.startPreview();
+                mCamera.takePicture(null, null, mPicture);
+                mCamera.stopPreview();
+                mCamera.startPreview();
+            }
+        }, 1000, 10000);
+
+    }
+
+    /**
+     * Helper method to access the camera returns null if it cannot get the
+     * camera or does not exist
+     *
+     * @return
+     */
+    private Camera getCameraInstance() {
+        Camera camera = null;
+        try {
+            camera = Camera.open(1);
+        } catch (Exception e) {
+            // cannot get camera or does not exist
+        }
+        return camera;
+    }
+
+    PictureCallback mPicture = new PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null) {
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data , 0, data .length);
+                File file = createTempFile(bitmap);
+                RequestBody rb = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+                MultipartBody.Part mb = MultipartBody.Part.createFormData("file", file.getName(), rb);
+                update(rb, mb);
+
+            } catch (FileNotFoundException e) {
+
+            } catch (IOException e) {
+            }
+        }
+
+    };
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+                .format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    private void update(final RequestBody fileName, MultipartBody.Part file) {
+        mApiService.uploadPhoto(file, fileName)
+                .enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if (response.isSuccessful()){
+                            Log.i("debug", "onResponse: BERHASIL"+response);
+                            try {
+                                JSONObject jsonRESULTS = new JSONObject(response.body().string());
+                                String stts = jsonRESULTS.getString("expression");
+                                Log.i("tesuplod","tes: "+stts);
+                                tvText.setText(stts);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            Log.i("debug", "onResponse: GA BERHASIL "+response);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Log.e("debug", "onFailure: ERROR > " + t.getMessage());
+                    }
+                });
+    }
+
+    private File createTempFile(Bitmap bitmap) {
+        File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                , System.currentTimeMillis() +"_image.png");
+        Matrix rotateMatrix = new Matrix();
+        rotateMatrix.postRotate(-90);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0,
+                bitmap.getWidth(), bitmap.getHeight(),
+                rotateMatrix, false);
+        Bitmap bitmapGray = toGrayscale(rotatedBitmap);
+        Bitmap resized = Bitmap.createScaledBitmap(bitmapGray, 48, 48, true);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        resized.compress(Bitmap.CompressFormat.JPEG,100, bos);
+        byte[] bitmapdata = bos.toByteArray();
+        //write the bytes in file
+
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(bitmapdata);
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    public Bitmap toGrayscale(Bitmap bmpOriginal)
+    {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
+}
---
+++ Client/app/src/main/java/com/example/faceanalytic/apiHelper/BaseApiService.java
@@ -0,0 +1,16 @@
+package com.example.faceanalytic.apiHelper;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface BaseApiService {
+    @Multipart
+    @POST("upload_photo")
+    Call<ResponseBody> uploadPhoto(@Part MultipartBody.Part file,
+                                   @Part("file") RequestBody fileName);
+}
