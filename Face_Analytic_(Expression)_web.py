14d7be4c92d9644701ac194e212d56dbeb99c797
diahputri90 <62321815+diahputri90@users.noreply.github.com>
2020-06-21 21:18:51 +0800

    Update Face_Analytic_(Expression)_web.py

--- Face_Analytic_(Expression)_web.py
+++ Face_Analytic_(Expression)_web.py
@@ -1,7 +1,7 @@
 """
 Face Analytic (Expression) with TensorFlow  
   
-Copyright 2020  I Made Agus Dwi Suarjaya, Author 2, Author 3, Author 4  
+Copyright 2020  I Made Agus Dwi Suarjaya, Author 2, Author 3, Ni Luh Putu Diah Putri Maheswari 
   
 Description     : Try to analyze faces with TensorFlow and classify into 7 expressions (angry, disgust, fear, happy, neutral, sad, surprise)  
 Dataset source  : https://www.kaggle.com/jonathanoheix/face-expression-recognition-dataset
